9b18936e12171015e723ccda8ae1c4d8a4ca6b33
qudryash <qudryashovav@yandex.ru>
2023-11-05 19:59:03 +0700

    Добавил новую ветку. Изменил "Первый код" на "Главная ветка -> Ветка 1"

--- GitHub/ViewController.swift
+++ GitHub/ViewController.swift
@@ -11,7 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print ("Первый код")
+        print ("Главная ветка -> Ветка 1")
     }
 
 
